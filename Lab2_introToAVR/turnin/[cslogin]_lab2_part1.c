c300b96971895498a7bc208a6f546479e332d36c
Steve Cheung <scheu010@wch136-25.cs.ucr.edu>
2019-10-04 16:37:41 -0700

    Completed part 1

--- Lab2_introToAVR/turnin/[cslogin]_lab2_part1.c
+++ Lab2_introToAVR/turnin/[cslogin]_lab2_part1.c
@@ -15,10 +15,12 @@
 int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+	unsigned char temp = 0x00;
 	
 	while(1) {
 		
-		if (PINA == 0x01) {
+		temp = PINA;
+		if (temp == 0x01) {
 			PORTB = 0x01;
 		} else {
 			PORTB = 0x00;
