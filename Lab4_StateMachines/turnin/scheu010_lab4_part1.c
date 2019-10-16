8878c96e0de261599f7d4416556c0230e0d5a589
Garlocks <scheu010@ucr.edu>
2019-10-16 09:14:20 -0700

    Update scheu010_lab4_part1.c

--- Lab4_StateMachines/turnin/scheu010_lab4_part1.c
+++ Lab4_StateMachines/turnin/scheu010_lab4_part1.c
@@ -12,33 +12,87 @@
 #include "simAVRHeader.h"
 #endif
 
+
+enum States {BEGIN, INIT, OFF, WAIT1, ON, WAIT2}state;
+void Tick();
 int main(void) {
-	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-	DDRB = 0x00; PORTB = 0xFF; // Configure port B's 8 pins as inputs
-	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
-	unsigned char tempa = 0x00;
-	unsigned char tempb = 0x00;
-	unsigned char tempc = 0x00;
-	unsigned char num1a = 0x00;
-	int i = 0;
-	
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x01;
+	state = Begin;
 	while(1) {
-		tempa = PINA;
-		tempb = PINB;
-		num1a = 0x00;
-		for (i = 0; i < 8; ++i) {
-			if (tempa & 0x01 == 0x01) {
-				num1a = num1a + 1;
-			}
-			if (tempb & 0x01 == 0x01) {
-				num1a = num1a + 1;
-			}
-			tempa = tempa >> 1;
-			tempb = tempb >> 1;
+		Tick();
+	}
+}
+
+void Tick() {
+	switch(state) { 
+		case Begin:
+		PORTB = 0x01;
+		state = INIT;
+		break;
+		
+		case INIT:
+		if((~PINA & 0x01) == 0x01) {
+			state = OFF; break;
+		} else {
+			state = INIT; break;
 		}
-		tempc = num1a;
-		PORTC = tempc;
+		
+		case OFF:
+		if((~PINA & 0x01) == 0x00) {
+			state = WAIT1; break;
+		} else {
+			state = OFF; break;
+		}
+		
+		case WAIT1:
+		if((~PINA & 0x01) == 0x01) {
+			state = ON; break;
+		} else {
+			state = WAIT1; break;
+		}
+		
+		case ON:
+		if((~PINA & 0x01) == 0x00) {
+			state = WAIT2; break;
+		} else {
+			state = ON; break;
+		}
+		
+		case WAIT2:
+		if((~PINA & 0x01) == 0x01) {
+			state = OFF; break;
+		} else {
+			state = WAIT2; break;
+		}
+		
+		default:
+		break;
 	}
 	
-	return 0;
+	switch(state) { 
+		case Begin:
+		break;
+		
+		case INIT:
+		PORTB = 0x01;
+		break;
+		
+		case OFF:
+		PORTB = 0x02;
+		break;
+		
+		case WAIT1:
+		break;
+		
+		case ON:
+		PORTB = 0x01;
+		break;
+		
+		case WAIT2:
+		break;
+		
+		default:
+		break;
+	}
 }
