c88220bf1b5aeb8af930fcd25ed0cc8254ae5689
Garlocks <scheu010@ucr.edu>
2019-10-16 09:28:05 -0700

    Update scheu010_lab4_part3.c

--- Lab4_StateMachines/turnin/scheu010_lab4_part3.c
+++ Lab4_StateMachines/turnin/scheu010_lab4_part3.c
@@ -12,43 +12,104 @@
 #include "simAVRHeader.h"
 #endif
 
+enum States {START, INIT, HASHTAG, WAIT, UNLOCK, LOCK, PAUSE}state;
+void Tick();
+
 int main(void) {
-	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
-	unsigned char temp = 0x00;
-	unsigned char output = 0x00;
-	
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x02;
+	DDRC = 0xFF; PORTC = 0x00;
+	state = START;
 	while(1) {
-		
-		temp = PINA;
-		output = 0x00;
-		if (temp == 0x01 || temp == 0x02 || temp == 0x31 || temp == 0x32) {
-			output = 0x60; //low fuel
-		}
-		if (temp == 0x03 || temp == 0x04 || temp == 0x33 || temp == 0x34) {
-			output = 0x70; //low fuel
-		}
-		if (temp == 0x05 || temp == 0x06 || temp == 0x35 || temp == 0x36) {
-			output = 0x38;
-		}
-		if (temp == 0x07 || temp == 0x08 || temp == 0x09 || temp == 0x37 || temp == 0x38 || temp == 0x39) {
-			output = 0x3C;
-		}
-		if (temp == 0x0A || temp == 0x0B || temp == 0x0C || temp == 0x3A || temp == 0x3B || temp == 0x3C) {
-			output = 0x3E;
-		}
-		if (temp == 0x0D || temp == 0x0E || temp == 0x0F || temp == 0x3D || temp == 0x3E || temp == 0x3F) {
-			output = 0x3F;
-		}
-		if ((temp & 0x30) == 0x30) {
-			output =  (output + 0x80);
+		Tick();
+	}
+}
+
+void Tick() {
+	switch(state) { 
+		case START: {
+			state = INIT;
+			break;
 		}
 		
-		PORTC = output;
+		case INIT:
+		if((~PINA & 0x04) == 0x04) {
+			state = HASHTAG; break;
+		} else if ((~PINA & 0x80) == 0x80) {
+			state = LOCK; break;
+		} else {
+			state = INIT; break;
+		}
+		
+		case HASHTAG: {
+			state = WAIT; break;
+		}
+		
+		case WAIT: {
+			if((~PINA & 0x02) == 0x02) {
+				state = UNLOCK; break;
+			} else if((~PINA & 0x80) == 0x80) {
+				state = LOCK; break;
+			} else if((~PINA & 0x01) == 0x01) {
+				state = INIT; break;
+			} else {
+				state = WAIT; break;
+			}
+		}
 		
 		
+		case UNLOCK: {
+			state = PAUSE; break;
+		}
 		
+		case PAUSE:
+		if((~PINA & 0x87) == 0x00) {
+			state = INIT; break;
+		} else {
+			state = PAUSE; break;
+		}
+		
+		case LOCK:
+		if((~PINA & 0x00) == 0x00) {
+			state = INIT; break;
+		} else {
+			state = LOCK; break;
+		}
+		
+		default:
+		break;
+	}
+	switch(state) { 
+		case START:
+		PORTB = 0x02;
+		PORTC = 0x00;
+		break;
+		
+		case INIT:
+		PORTC = 0x01;
+		break;
+		
+		case HASHTAG:
+		PORTC = 0x02;
+		break;
+		
+		case WAIT:
+		PORTC = 0x03;
+		break;
+		
+		case UNLOCK:
+		PORTC = 0x04;
+		PORTB = 0x01;
+		break;
+		
+		case PAUSE:
+		PORTC = 0x05;
+		break;
+		
+		case LOCK:
+		PORTC = 0x06;
+		PORTB = 0x02;
+		break;
 	}
 	
-	return 0;
 }
