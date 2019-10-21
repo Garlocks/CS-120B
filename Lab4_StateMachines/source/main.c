d34bc3e1485baa31c031cc76bc5acba9ff00affc
Garlocks <scheu010@ucr.edu>
2019-10-21 05:56:52 -0700

    Update main.c

--- Lab4_StateMachines/source/main.c
+++ Lab4_StateMachines/source/main.c
@@ -12,98 +12,90 @@
 #include "simAVRHeader.h"
 #endif
 
-enum States {BEGIN, INIT, ADD, MINUS, WAIT, RESET}state;
-void Tick();
 
+enum States {BEGIN, INIT, OFF, WAIT1, ON, WAIT2}state;
+void Tick();
 int main(void) {
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRC = 0xFF; PORTC = 0x07;
+	DDRB = 0xFF; PORTB = 0x01;
 	state = BEGIN;
 	while(1) {
 		Tick();
 	}
 }
 
-void Tick(){
-	switch(state){ 
-		case BEGIN: {
-			PORTC = 0x07;
-			state = INIT;
-			break;
-		}
+void Tick() {
+	switch(state) { 
+		case BEGIN:
+		PORTB = 0x01;
+		state = INIT;
+		break;
 		
 		case INIT:
-		if (PINA  == 0x01) {
-			state = ADD; break;
-		} else if(PINA== 0x02) {
-			state = MINUS; break;
-		} else if(PINA == 0x03) {
-			state = RESET; break;
-		} else if (PINA == 0x00) {
-			state = INIT; break;
-		}
-		break;
-		
-		case ADD:
-		state = WAIT;
-		break;
-		
-		case MINUS:
-		state = WAIT;
-		break;
-		
-		case WAIT:
-		if((PINA== 0x01) || (PINA == 0x02)) {
-			state = WAIT; break;
-		}
-		else if((PINA & 0x03) == 0x03) {
-			state = RESET; break;
+		PORTB = 0x01;
+		if(PINA == 0x01) {
+			state = OFF; break;
 		} else {
 			state = INIT; break;
 		}
 		
-		case RESET:
-		if((PINA == 0x01) && (PINA == 0x02)) {
-			state = RESET; break;
-		} else if (PINA == 0x00) {
-			state = INIT; break;
+		case OFF:
+		PORTB = 0x02;
+		if(PINA == 0x00) {
+			state = WAIT1; break;
+		} else {
+			state = OFF; break;
+		}
+		
+		case WAIT1:
+		if(PINA == 0x01) {
+			state = ON; break;
+		} else {
+			state = WAIT1; break;
+		}
+		
+		case ON:
+		PORTB = 0x01;
+		if(PINA == 0x00) {
+			state = WAIT2; break;
+		} else {
+			state = ON; break;
+		}
+		
+		case WAIT2:
+		if(PINA == 0x01) {
+			state = OFF; break;
+		} else {
+			state = WAIT2; break;
 		}
 		
 		default:
 		break;
 	}
+	
 	switch(state) { 
-		case BEGIN: {
-			PORTC = 0x07;
-		}
+		case BEGIN:
 		break;
 		
 		case INIT:
+		PORTB = 0x01;
 		break;
 		
-		case ADD: {
-			if(PORTC >= 0x09) {
-				PORTC = 0x09; break;
-			} else {
-				PORTC = PORTC + 0x01; break;
-			}
-		}
+		case OFF:
+		PORTB = 0x02;
 		break;
 		
-		case MINUS: {
-			if(PORTC <= 0x00) {
-				PORTC = 0x00; break;
-			} else {
-				PORTC = PORTC - 0x01; break;
-			}
-		}
+		case WAIT1:
 		break;
 		
-		case WAIT:
+		case ON:
+		PORTB = 0x01;
 		break;
 		
-		case RESET: {
-			PORTC = 0x00; break;
-		}
+		case WAIT2:
+		break;
+		
+		default:
+		break;
 	}
 }
