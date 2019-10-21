909ccc3f7f358be8aa91bcae8852b802165142a4
Garlocks <scheu010@ucr.edu>
2019-10-21 06:30:14 -0700

    Update main.c

--- Lab4_StateMachines/source/main.c
+++ Lab4_StateMachines/source/main.c
@@ -12,13 +12,14 @@
 #include "simAVRHeader.h"
 #endif
 
-
-enum States {BEGIN, INIT, OFF, WAIT1, ON, WAIT2}state;
+enum States {START, INIT, HASHTAG, WAIT, UNLOCK, LOCK, PAUSE}state;
 void Tick();
+
 int main(void) {
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x01;
-	state = BEGIN;
+	DDRB = 0xFF; PORTB = 0x02;
+	DDRC = 0xFF; PORTC = 0x00;
+	state = START;
 	while(1) {
 		Tick();
 	}
@@ -26,76 +27,89 @@ int main(void) {
 
 void Tick() {
 	switch(state) { 
-		case BEGIN:
-		PORTB = 0x01;
-		state = INIT;
+		case START: {
+			state = INIT;
+			break;
+		}
+		
+		case INIT:
+		if((~PINA & 0x04) == 0x04) {
+			state = HASHTAG; break;
+		} else if ((~PINA & 0x80) == 0x80) {
+			state = LOCK; break;
+		} else {
+			state = INIT; break;
+		}
+		
+		case HASHTAG: {
+			state = WAIT; break;
+		}
+		
+		case WAIT: {
+			if((~PINA & 0x02) == 0x02) {
+				state = UNLOCK; break;
+			} else if((~PINA & 0x80) == 0x80) {
+				state = LOCK; break;
+			} else if((~PINA & 0x01) == 0x01) {
+				state = INIT; break;
+			} else {
+				state = WAIT; break;
+			}
+		}
+		
+		
+		case UNLOCK: {
+			state = PAUSE; break;
+		}
+		
+		case PAUSE:
+		if((~PINA & 0x87) == 0x00) {
+			state = INIT; break;
+		} else {
+			state = PAUSE; break;
+		}
+		
+		case LOCK:
+		if((~PINA & 0x00) == 0x00) {
+			state = INIT; break;
+		} else {
+			state = LOCK; break;
+		}
+		
+		default:
+		break;
+	}
+	switch(state) { 
+		case START:
+		PORTB = 0x02;
+		PORTC = 0x00;
 		break;
 		
 		case INIT:
-		PORTB = 0x01;
-		if(PINA == 0x01) {
-			state = OFF; break;
-		} else {
-			state = INIT; break;
-		}break;
+		PORTC = 0x01;
+		break;
 		
-		case OFF:
+		case HASHTAG:
+		PORTC = 0x02;
+		break;
+		
+		case WAIT:
+		PORTC = 0x03;
+		break;
+		
+		case UNLOCK:
+		PORTC = 0x04;
+		PORTB = 0x01;
+		break;
+		
+		case PAUSE:
+		PORTC = 0x05;
+		break;
+		
+		case LOCK:
+		PORTC = 0x06;
 		PORTB = 0x02;
-		if(PINA == 0x00) {
-			state = WAIT1; break;
-		} else {
-			state = OFF; break;
-		}break;
-		
-		case WAIT1:
-		if(PINA == 0x01) {
-			state = ON; break;
-		} else {
-			state = WAIT1; break;
-		}break;
-		
-		case ON:
-		PORTB = 0x01;
-		if(PINA == 0x00) {
-			state = WAIT2; break;
-		} else {
-			state = ON; break;
-		}break;
-		
-		case WAIT2:
-		if(PINA == 0x01) {
-			state = OFF; break;
-		} else {
-			state = WAIT2; break;
-		}break;
-		
-		default:
 		break;
 	}
 	
-	switch(state) { 
-		case BEGIN:
-		break;
-		
-		case INIT:
-		PORTB = 0x01;
-		break;
-		
-		case OFF:
-		PORTB = 0x02;
-		break;
-		
-		case WAIT1:
-		break;
-		
-		case ON:
-		PORTB = 0x01;
-		break;
-		
-		case WAIT2:
-		break;
-		
-		default:
-		break;
-	}
 }
