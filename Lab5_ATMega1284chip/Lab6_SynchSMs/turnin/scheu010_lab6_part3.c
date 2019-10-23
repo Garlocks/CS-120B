78048fb5d53b12ac9db88c28c4f9346bb1d980a6
Garlocks <scheu010@ucr.edu>
2019-10-23 12:36:08 -0700

    Add files via upload

---
+++ Lab5_ATMega1284chip/Lab6_SynchSMs/turnin/scheu010_lab6_part3.c
@@ -0,0 +1,222 @@
+/*	Author: scheu010
+ *  Partner(s) Name: vivian
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+volatile unsigned char TimerFlag = 0; //timerISR() sets this to 1. C programmer should clear to 0
+
+//internal variables for mapping AVR's ISR to our cleaner timerISR model
+unsigned long _avr_timer_M = 1; //start count from here, down to 0. default 1ms
+unsigned long _avr_timer_cntcurr = 0; //current internal count of 1ms ticks
+
+
+void TimerOn() {
+	//avr timer/counter controller register TCCR1
+	TCCR1B = 0x0B; //bit3=0: CTC mode (clear timer on compare)
+
+	//avr output compare register OCR1A
+	OCR1A = 125; //timer interrupt will be generated when TCNT1==OCR1A
+
+	//avr timer interrupt mask register
+	TIMSK1 = 0x02; //bit1: OCIE1A -- enables match interrupt
+
+	//initialize avr counter
+	TCNT1=0;
+
+	_avr_timer_cntcurr = _avr_timer_M;
+	//timerISR will be called every _avr_timer_cntcurr millisecs
+
+	//enable glibal interrupts
+	SREG |= 0x80; 
+}
+
+void TimerOff() {
+	TCCR1B = 0x00; 
+}
+
+void TimerISR() {
+	TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect) {
+	_avr_timer_cntcurr--;
+	if (_avr_timer_cntcurr == 0)
+	{
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+
+//set timerISR() to tick every M ms
+void TimerSet(unsigned long M)
+{
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+enum States {Start, INIT, ADD, MINUS, RESET, ADDING, MINUSING}state;
+unsigned char temp = 0x00;
+
+void Tick() {
+	switch(state) { //transitions
+		case Start:
+			PORTB = 0x07;
+			temp = 0x00;
+			state = INIT;
+			break;
+
+		case INIT:
+			if ((~PINA & 0x03) == 0x01) {
+				state = ADD;
+			} else if ((~PINA & 0x03) == 0x02) {
+				state = MINUS;
+			} else if ((~PINA & 0x03) == 0x03) {
+				state = RESET;
+			} else if ((~PINA & 0x03) == 0x00) {
+				state = INIT;
+			}
+			break;
+
+		case ADD: 
+			if ((~PINA & 0x03) == 0x01) {
+				state = ADDING;
+			} else if ((~PINA & 0x03) == 0x02) {
+				state = MINUS;
+			} else if ((~PINA & 0x03) == 0x03) {
+				state =  RESET;
+			} else if ((~PINA & 0x03) == 0x00) {
+				state = INIT;
+			}
+			break;
+
+		case MINUS: 
+			if ((~PINA & 0x03) == 0x01) {
+				state = ADD;
+			} else if ((~PINA & 0x03) == 0x02) {
+				state = MINUSING;
+			} else if ((~PINA & 0x03) == 0x03) {
+				state =  RESET;
+			} else if ((~PINA & 0x03) == 0x00) {
+				state = INIT;
+			}
+			break;
+
+		case RESET:
+			if ((~PINA & 0x03) == 0x01) {
+				state = ADD;
+			} else if ((~PINA & 0x03) == 0x02) {
+				state = MINUS;
+			} else if ((~PINA & 0x03) == 0x03) {
+				state =  RESET;
+			} else if ((~PINA & 0x03) == 0x00) {
+				state = INIT;
+			}
+			break;
+			
+		case ADDING: 
+			if ((~PINA & 0x03) == 0x01) {
+				state = ADDING;
+			} else if ((~PINA & 0x03) == 0x02) {
+				state = MINUS;
+			} else if ((~PINA & 0x03) == 0x03) {
+				state =  RESET;
+			} else if ((~PINA & 0x03) == 0x00) {
+				state = INIT;
+			}
+			break;
+		
+		case MINUSING: 
+			if ((~PINA & 0x03) == 0x01) {
+				state = ADD;
+			} else if ((~PINA & 0x03) == 0x02) {
+				state = MINUSING;
+			} else if ((~PINA & 0x03) == 0x03) {
+				state =  RESET;
+			} else if ((~PINA & 0x03) == 0x00) {
+				state = INIT;
+			}
+			break;
+	}
+		
+	switch(state){ //actions
+		case Start:
+			PORTB = 0x07;
+			temp = 0x00;
+			break;
+
+		case INIT:
+			temp = 0x00;
+			break;
+
+		case ADD: 
+			temp = 0x00;
+			if (PORTB < 0x09) {
+				PORTB = PORTB + 0x01;
+			}
+			break;
+
+		case MINUS: 
+			temp = 0x00;
+			if (PORTB > 0x00) {
+				PORTB = PORTB - 0x01;
+			}
+			break;
+
+		case RESET:
+			PORTB = 0x00;
+			temp = 0x00;
+			break;
+			
+		case ADDING: 
+			if ((PORTB < 0x09) && (temp >= 0x0A)) {
+				PORTB = PORTB + 0x01;
+				temp = 0x00;
+			}
+			temp = temp + 0x01;
+			break;
+		
+		case MINUSING: 
+			if ((PORTB > 0x00) && (temp >= 0x0A)) {
+				PORTB = PORTB - 0x01;
+				temp = 0x00;
+			}
+			temp = temp + 0x01;
+			break;
+	}
+}
+
+int main(void)
+{
+	DDRA = 0x00;
+	PORTA = 0xFF;
+	DDRB = 0xFF;
+	PORTB = 0x00;
+
+	state = Start;
+	TimerSet(100);
+	TimerOn();
+	while(1) {
+		Tick();
+		while (!TimerFlag);
+		TimerFlag = 0;
+	}
+}
+
+
+
+
+
